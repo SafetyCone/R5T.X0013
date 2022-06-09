556fae1b2bbddbea70b0b56f296155093e150544
davidcoats <david@getsafetycone.com>
2022-06-09 12:43:29 -0700

    Interim changes.

---
+++ source/R5T.X0013/Code/Extensions/ISyntaxNodeAnnotationExtensions-Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using R5T.T0134;
+
+
+namespace System
+{
+    public static partial class ISyntaxNodeAnnotationExtensions
+    {
+        
+    }
+}
---
+++ source/R5T.X0013/Code/Extensions/ISyntaxNodeAnnotationExtensions-Namespace.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+using R5T.T0134;
+
+
+namespace System
+{
+    public static partial class ISyntaxNodeAnnotationExtensions
+    {
+        public static AddNodeResult<TParentNode, TMember> AddMemberWithResult<TParentNode, TMember>(this ISyntaxNodeAnnotation<NamespaceDeclarationSyntax> namespaceAnnotation,
+            TParentNode parentNode,
+            TMember member)
+            where TParentNode : SyntaxNode
+            where TMember : MemberDeclarationSyntax
+        {
+            member = member.Annotate(out var memberAnnotation);
+
+            parentNode = namespaceAnnotation.Modify(
+                parentNode,
+                @namespace => @namespace.AddMembers(member));
+
+            var output = AddNodeResult.From(
+                parentNode,
+                memberAnnotation);
+
+            return output;
+        }
+    }
+}
